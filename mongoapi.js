c82307a7dd2ee16109e41d04ab740fb7c1808dce
James Etchberger <jamesetchberger@Jamess-MacBook-Air.local>
2022-07-28 12:02:51 -0400

    added a get and a post

--- mongoapi.js
+++ mongoapi.js
@@ -1,8 +1,47 @@
+import { MongoClient } from 'mongodb'
+import { uri } from './dbsecrets.js'
 import express from 'express'
 import cors from 'cors'
+
+const client = new MongoClient(uri) //connects to db
+const db = client.db('sample_mflix') //finds collection
+const movieCollection = db.collection('movies')
 
 const app = express()
 const PORT = 4000
 
-app.use(express.json())
-app.use(cors())
+app.use(express.json()) // allow a POST with JSON
+app.use(cors()) //allow anyone to hit this api this is a security thing
+
+// if you go to hhtp://localhost:4000/
+app.get('/', (req, res) => {
+    res.status(200).send('Hello World! ... again')
+})
+
+
+// if you go to hhtp://localhost:4000/movies
+app.get('/movies', (req, res) => {
+  const query = {}
+  //console.log(movieCollection.countDocuments(query)) //this will help find the problem if the code isnt bringing anything up
+  movieCollection
+    .find(query)
+    .limit(10)
+    .toArray((err, movies) => {
+      res.status(200).json(movies)
+    })
+})
+
+app.listen(PORT, () => {
+  console.log(`Up and running on port ${PORT}`)
+})
+
+app.post('/movie', (req,res) => {
+    const newMovie = req.body
+    movieCollection.insertOne(newMovie, (err, results) => {
+        if (err) {
+            res.status(500).json({error: true})
+        } else {
+            res.status(201).json(results)
+        }
+    })
+})
